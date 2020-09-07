3e8606186c1fe9dbab8d0750ba1b7426c61702c7
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-09-08 04:14:38 +0500

    Deploy Quiz_App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getQuizDetails } from './services/quiz_service';
+import { QuestionType, Quiz } from '././Types/quiz_types'
+import QuestionCard from './Components/QuestionCards';
 
 function App() {
+  let [quiz, setQUiz] = useState<QuestionType[]>([])
+  let [currentStep, setCurrentStep] = useState(0)
+  
+  useEffect(() => {
+    async function fetchData() {
+      const questions: QuestionType[] = await getQuizDetails(3,"easy");
+      console.log(questions);
+      setQUiz(questions);
+    }
+    fetchData();
+  }, []);
+
+  const handleSubmit = (e:React.FormEvent<EventTarget>) =>{
+    e.preventDefault();
+    if(currentStep!== quiz.length-1)
+    setCurrentStep(++currentStep);
+    else {alert("Quiz Completed");
+  }
+  }
+
+  if (!quiz.length)
+    return <h3>Loading...</h3>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuestionCard
+        options={quiz[currentStep].option}
+        question={quiz[currentStep].question}
+        callback={handleSubmit}
+      />
     </div>
   );
 }
