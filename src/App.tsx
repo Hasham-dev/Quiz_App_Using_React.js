a340e72ac8c03f61b9a1245dd78a166c127fbfcb
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-09-10 00:46:00 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { getQuizDetails,Difficulty } from './services/quiz_service';
+import { getQuizDetails, Difficulty } from './services/quiz_service';
 import { QuestionType } from '././Types/quiz_types'
 import QuestionCard from './Components/QuestionCards';
 import Container from '@material-ui/core/Container';
@@ -10,10 +10,10 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init(
-    {
-        duration: 2000, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-    }
+  {
+    duration: 2000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+  }
 );
 
 function App() {
@@ -51,36 +51,36 @@ function App() {
   if (!quiz.length)
     return <h3>Loading...</h3>
 
-  if(showResult){
-    return(
+  if (showResult) {
+    return (
       <Container maxWidth="sm" className="container">
-      <div>
-      <Paper elevation={3} className="Paper" data-aos="fade-up">
-        <div className="result">
-        <h1 data-aos="fade-right"
-     data-aos-offset="300"
-     data-aos-easing="ease-in-sine">Results</h1>
-        <h2 data-aos="fade-left"
-     data-aos-offset="300"
-     data-aos-easing="ease-in-sine">Your Final Score is</h2>
-        <h3 data-aos="fade-right"
-     data-aos-offset="300"
-     data-aos-easing="ease-in-sine"><b>{score}</b> Out Of <b>{quiz.length}</b></h3>
-        <Button
-        onClick={()=>{
-          setCurrentStep(0)
-          setScore(0)
-          setShowResult(false)
-        }}
-        className="Button"
-        >Reset</Button>
+        <div>
+          <Paper elevation={3} className="Paper" data-aos="fade-up">
+            <div className="result">
+              <h1 data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine">Results</h1>
+              <h2 data-aos="fade-left"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine">Your Final Score is</h2>
+              <h3 data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"><b>{score}</b> Out Of <b>{quiz.length}</b></h3>
+              <Button
+                onClick={() => {
+                  setCurrentStep(0)
+                  setScore(0)
+                  setShowResult(false)
+                }}
+                className="Button"
+              >Reset</Button>
+            </div>
+          </Paper>
         </div>
-        </Paper>
-      </div>
       </Container>
     )
   }
-    
+
   return (
     <div className="App">
       <h1>Quiz App</h1>
